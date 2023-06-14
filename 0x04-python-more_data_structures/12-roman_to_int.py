15f0dfce3e09fc2a94029d5071920b3f3da12abb
Yab8702 <yaleletdessalegn11@gmail.com>
2023-06-14 14:06:41 +0000

    roman numerals one less than five

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,9 +6,13 @@ def roman_to_int(roman_string):
                 'I': 1, 'V': 5, 'X': 10, 'L': 50,
                 'C': 100, 'D': 500, 'M': 1000
                 }
-        for s_str in roman_string:
+        for s_str, i in zip(roman_string, range(len(roman_string))):
             if roman_dic.get(s_str, 0) == 0:
                 return 0
-            number += roman_dic[s_str]
+            if i != len(roman_string) - 1 and \
+                    roman_dic[s_str] < roman_dic[roman_string[i + 1]]:
+                number += roman_dic[roman_string[i]]-roman_dic[s_str]
+            else:
+                number += roman_dic[s_str]
         return number
     return 0
