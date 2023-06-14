fb565c6f371cdc93cfb49a827398a7397c228559
Yab8702 <yaleletdessalegn11@gmail.com>
2023-06-14 18:07:24 +0000

    Advanced Task

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+*/
+
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size = PyList_Size(p);
+	PyListObject *list = (PyListObject *) p;
+	Py_ssize_t i;
+	PyObject *item;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+	for (i = 0; i < size; i++)
+	{
+		item = PySequence_GetItem(p, i);
+		printf("Element %ld: %s\n", i, item->ob_type->tp_name);
+		Py_DECREF(item);
+	}
+}
+
+/**
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: A PyObject byte object.
+ */
+
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	Py_ssize_t size = ((PyVarObject *)p)->ob_size;
+	Py_ssize_t i, limit;
+
+	printf("[.] bytes object info\n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+	limit = size < 10 ? size : 10;
+	printf("  first %ld bytes: ", limit);
+	for (i = 0; i < limit; i++)
+	{
+		printf("%02hhx", bytes->ob_sval[i]);
+		if (i < limit - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+}
