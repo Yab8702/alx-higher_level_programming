1c48593d147446b1c346334268f24d65d36f44ef
Yab8702 <yaleletdessalegn11@gmail.com>
2023-07-10 10:04:02 +0000

    python inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class MyList(list):
+    """Implements MyList Class"""
+
+    def print_sorted(self):
+        """Print a sorting list"""
+
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """Represent Base Geometry."""
+
+    def area(self):
+        """Not Yet Implemented"""
+
+        raise Exception("area() is not implemented")
