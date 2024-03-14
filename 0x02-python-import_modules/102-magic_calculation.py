d3cae161f7d46ec82c63b9db67b327ff69128488
yab8702 <yaleletdessalegn11@gmail.com>
2024-03-14 06:49:23 -0700

    x02-python-import_modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/paython3
+""" byte code is so awesome """
+
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return (sub(a, b))
